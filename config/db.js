1a4da9401966254d663b4171f12891c10d699cb4
odaymare02 <mareoday123@gmail.com>
2026-01-01 17:51:09 +0200

    finalUpdatedj

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
+// db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI); // لا خيارات إضافية
         console.log("MongoDB Connected");
     } catch (err) {
         console.error("MongoDB connection error:", err);
